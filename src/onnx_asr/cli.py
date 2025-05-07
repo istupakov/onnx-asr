1d7a5a9ce262318a46da12bb43c0bfec8e9f3c07
Ilya Stupakov <istupakov@gmail.com>
2025-05-07 17:52:38 +0000

    Update CLI (add VAD, fix model names)

--- src/onnx_asr/cli.py
+++ src/onnx_asr/cli.py
@@ -2,10 +2,11 @@
 
 import argparse
 import pathlib
+from importlib.metadata import version
 from typing import get_args
 
-from onnx_asr import load_model
-from onnx_asr.loader import ModelNames, ModelTypes
+import onnx_asr
+from onnx_asr.loader import ModelNames, ModelTypes, VadNames
 
 
 def run() -> None:
@@ -13,8 +14,7 @@ def run() -> None:
     parser = argparse.ArgumentParser(prog="onnx_asr", description="Automatic Speech Recognition in Python using ONNX models.")
     parser.add_argument(
         "model",
-        choices=get_args(ModelNames) + get_args(ModelTypes),
-        help="Model name or type",
+        help=f"Model name or type {(*get_args(ModelNames), *get_args(ModelTypes), 'onnx-community/whisper-...')}",
     )
     parser.add_argument(
         "filename",
@@ -23,8 +23,17 @@ def run() -> None:
     )
     parser.add_argument("-p", "--model_path", type=pathlib.Path, help="Path to directory with model files")
     parser.add_argument("-q", "--quantization", help="Model quantization ('int8' for example)")
+    parser.add_argument("--vad", help="Use VAD model", choices=get_args(VadNames))
+    parser.add_argument("--version", action="version", version=f"%(prog)s {version('onnx_asr')}")
     args = parser.parse_args()
 
-    model = load_model(args.model, args.model_path, quantization=args.quantization)
-    for text in model.recognize(args.filename):
-        print(text)
+    model = onnx_asr.load_model(args.model, args.model_path, quantization=args.quantization)
+    if args.vad:
+        vad = onnx_asr.load_vad(args.vad)
+        for segment in model.with_vad(vad, batch_size=1).recognize(args.filename):
+            for res in segment:
+                print(f"[{res.start:5.1f}, {res.end:5.1f}]: {res.text}")
+            print()
+    else:
+        for text in model.recognize(args.filename):
+            print(text)
