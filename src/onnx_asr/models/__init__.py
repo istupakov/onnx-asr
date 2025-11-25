4ac77bab529e54e7012ad2a86b95cf03be784595
Ilya Stupakov <istupakov@gmail.com>
2025-11-25 19:57:28 +0000

    Add GigaAM v3 E2E models

--- src/onnx_asr/models/__init__.py
+++ src/onnx_asr/models/__init__.py
@@ -1,6 +1,6 @@
 """ASR model implementations."""
 
-from .gigaam import GigaamV2Ctc, GigaamV2Rnnt
+from .gigaam import GigaamV2Ctc, GigaamV2Rnnt, GigaamV3E2eCtc, GigaamV3E2eRnnt
 from .kaldi import KaldiTransducer
 from .nemo import NemoConformerCtc, NemoConformerRnnt, NemoConformerTdt
 from .pyannote import PyAnnoteVad
@@ -10,6 +10,8 @@ from .whisper import WhisperHf, WhisperOrt
 __all__ = [
     "GigaamV2Ctc",
     "GigaamV2Rnnt",
+    "GigaamV3E2eCtc",
+    "GigaamV3E2eRnnt",
     "KaldiTransducer",
     "NemoConformerCtc",
     "NemoConformerRnnt",
