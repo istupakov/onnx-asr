e69dc48d5c4b6553909c2c4a7c414a2890a553a0
Ilya Stupakov <istupakov@gmail.com>
2026-01-22 03:07:33 +0000

    Update loader tests and fix model caching

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import huggingface_hub as hf
+import numpy as np
+import onnxruntime as rt
+
+
+def pytest_report_header() -> str:
+    return f"onnx-asr deps: numpy-{np.__version__}, onnxruntime-{rt.__version__}, huggingface-hub-{hf.__version__}"
